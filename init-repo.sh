397a2e2ee87086d969c1d94a632c3734b88cac59
Vlad Serhiienko <vlad.serhiienko@gmail.com>
2025-03-16 01:13:53 -0700

    Fixes.

--- init-repo.sh
+++ init-repo.sh
@@ -1,9 +1,9 @@
 
-python -m venv venv
+python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
 
-mkdir conan_build
+mkdir -p conan_build
 cd conan_build
 
 conan_args="-s build_type=Debug -o with_raylib_examples=True -o with_sdl2_examples=True -o with_cpp_example=True -o with_demos=True -pr:b=../profiles/linux_x86_64 -pr:h=../profiles/linux_x86_64"
