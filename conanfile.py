02bf095ff5c5773cbd9d14f193f8134d1562e6fb
Vlad Serhiienko <vlad.serhiienko@gmail.com>
2025-03-16 01:10:37 -0700

    Enable conan build

---
+++ conanfile.py
@@ -0,0 +1,46 @@
+
+import os
+from conan import ConanFile
+from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout
+
+class Clay(ConanFile):
+    name = "clay"
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "CMakeDeps", "CMakeToolchain"
+
+    options = {
+        "with_raylib_examples": [True, False],
+        "with_sdl2_examples": [True, False],
+        "with_cpp_example": [True, False],
+        "with_demos": [True, False],
+    }
+
+    default_options = {
+        "with_raylib_examples": False,
+        "with_sdl2_examples": False,
+        "with_cpp_example": False,
+        "with_demos": False,
+    }
+
+    def requirements(self):
+        self.requires("raylib/5.5")
+        self.requires("sdl/2.28.3")
+        self.requires("sdl_ttf/2.24.0")
+        self.requires("sdl_image/2.8.2")
+        # self.requires("freetype/2.13.3", override=True)
+        # self.requires("sdl/3.2.6")
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure(variables={
+            # Disable all examples by default
+            "CLAY_INCLUDE_ALL_EXAMPLES": False,
+            # Temporary not supported due to missing packages
+            "CLAY_INCLUDE_SDL3_EXAMPLES": False,
+            # Enable supported examples based on options
+            "CLAY_INCLUDE_RAYLIB_EXAMPLES": self.options.with_raylib_examples,
+            "CLAY_INCLUDE_SDL2_EXAMPLES": self.options.with_sdl2_examples,
+            "CLAY_INCLUDE_CPP_EXAMPLE": self.options.with_cpp_example,
+            "CLAY_INCLUDE_DEMOS": self.options.with_demos,
+        })
+        cmake.build()
---
+++ init-repo.sh
@@ -0,0 +1,11 @@
+
+python -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+
+mkdir conan_build
+cd conan_build
+
+conan_args="-s build_type=Debug -o with_raylib_examples=True -o with_sdl2_examples=True -o with_cpp_example=True -o with_demos=True -pr:b=../profiles/linux_x86_64 -pr:h=../profiles/linux_x86_64"
+conan install .. -b missing ${conan_args}
+conan build .. -b missing ${conan_args}
