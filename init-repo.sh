532ace3180846b3854b17e5cd8b61efb9746fc6a
Vlad Serhiienko <vlad.serhiienko@gmail.com>
2025-03-19 00:00:46 -0700

    CI :ubuntu:

--- init-repo.sh
+++ init-repo.sh
@@ -4,12 +4,18 @@ python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
 
-conan create . \
-    --test-folder=test_package \
+mkdir -p build; cd build
+
+CONAN_ARGS="\
     -b missing \
-    -pr:b=profiles/linux_x86_64 \
-    -pr:h=profiles/linux_x86_64 \
+    -s build_type=Debug \
     -o clay_test/*:with_raylib_examples=True \
     -o clay_test/*:with_sdl2_examples=True \
     -o clay_test/*:with_cpp_example=True \
-    -o clay_test/*:with_demos=True
+    -o clay_test/*:with_demos=True \
+    -pr:b=../profiles/linux_x86_64 \
+    -pr:h=../profiles/linux_x86_64"
+
+conan install .. -of . ${CONAN_ARGS}
+conan build .. -of . ${CONAN_ARGS}
+conan create .. ${CONAN_ARGS}
